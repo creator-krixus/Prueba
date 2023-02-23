63851d56afd087541d90e3157f91d9c8ba9a0d8d
wilsonRueda <wilson.rueda@onegroupitservices.com>
2023-02-22 22:43:46 -0500

    Feature: New changes in web page

---
+++ src/components/Main.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="main">
+    <img class="main__image" src="../assets/ayahuasca.jpg"/>
+    <div class="main__info">
+        <h3 class="main__info--title">Conoce los beneficios de la ayahuasca o yagé</h3>
+        <ul>
+            <li>Promueve la salud del cerebro.</li>
+            <li>Genera bienestar psicológico.</li>
+            <li>Ayuda a superar la depresión y el trastorno de estrés postraumático.</li>
+        </ul>
+        <img class="main__info--image" src="../assets/ayahuasca_caldo.jpg"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main"
+};
+</script>
+<style lang="scss">
+.main{
+    background:#f1eae2;
+    &__image{
+        width: 100%;
+    }
+    &__info{
+        &--title{
+            text-align: center;
+        }
+        &--image{
+            width:100%;
+            border-top-left-radius: 80px;
+            border-bottom-right-radius: 80px;
+        }
+    }
+
+}
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="NavBar">
+    <img class="NavBar__logo" src="../assets/ayahuasca.jpeg" />
+    <h2>Ayahuascamed</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+}
+</script>
+<style lang="scss">
+.NavBar{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    background: #fcfbf9;
+    padding: 0px 5px 0px 5px;
+    &__logo{
+    width:60px;
+    border-radius: 50%
+}
+}
+
+
+</style>
